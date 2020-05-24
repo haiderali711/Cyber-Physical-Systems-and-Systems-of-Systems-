0e00365fc41399906d3f52d41d0da58ac0687a91
haiderali711 <gushaial@student.gu.se>
2020-05-24 15:20:45 +0200

    Fix bugs in the automatic scripts

--- BuildScript.sh
+++ BuildScript.sh
@@ -8,4 +8,4 @@
 
 #gnome-terminal --title="Auto-Steering" --command="bash -c 'docker build -t auto-steering:latest -f Dockerfile .';exec $SHELL"
 
-gnome-terminal --title="Builds" -e "bash -c 'docker build -t auto-steering:latest -f Dockerfile .;docker build -t pyhtongraph -f GraphDockerfile .;docker build https://github.com/chalmers-revere/opendlv-video-h264-decoder.git#v0.0.3 -f Dockerfile.amd64 -t h264decoder:v0.0.3;docker pull chalmersrevere/opendlv-vehicle-view-amd64:v0.0.60;exec $SHELL'"
+gnome-terminal --title="Builds" -e "bash -c 'docker build -t auto-steering:latest -f Dockerfile .;docker build -t pyhtongraph -f GraphDockerfile .;docker build https://github.com/chalmers-revere/opendlv-video-h264-decoder.git#v0.0.3 -f Dockerfile.amd64 -t h264decoder:v0.0.3;docker pull chalmersrevere/opendlv-vehicle-view-amd64:v0.0.60;docker system prune;exec $SHELL'"
